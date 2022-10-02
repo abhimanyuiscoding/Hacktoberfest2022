efbe01cd46b7894c6cd93fd00b29a131c93ae60e
unknown <omshaw21092001@gmail.com>
2022-10-02 21:42:14 +0530

    Added Kth Smallest Element in a BST program

--- Kth Smallest Element in a BST.cpp
+++ Kth Smallest Element in a BST.cpp
@@ -9,6 +9,15 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int& k) {
