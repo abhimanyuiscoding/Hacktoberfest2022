761274edf5332b835a66dda166e65ec37be8af6d
Akshat2024 <jaiswalakshat04@gmail.com>
2022-10-08 16:45:12 +0530

    Sum of Digits of a Nmber

---
+++ Sum_Of_Digits.cpp
@@ -0,0 +1,18 @@
+// [GITHUB ID -> (https://github.com/Akshat2024)]
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    // Taking input of the number from the user.
+    cout << "Enter the number" << endl;
+    cin >> n;
+    int sum = 0;
+    while (n > 0)
+    {
+        int p = n % 10;
+        sum = sum + p;
+        n = n / 10;
+    }
+    cout << sum << endl;
+}
