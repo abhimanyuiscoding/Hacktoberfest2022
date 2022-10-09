015066a0c6ec209436997c18a87cf13a08ba07e4
Heshani Nimalka <54610745+Heshani29@users.noreply.github.com>
2022-10-09 09:07:27 +0530

    Create sol.cpp

---
+++ print-problem/sol.cpp
@@ -0,0 +1,13 @@
+include <iostream>
+using namespace std;
+int main()
+{
+  int n;  
+  cout<<"Enter a value :\t";   
+  cin>>n;              
+    for(int i=1;i<=n;++i)
+    {              
+        cout<<i;                   
+    }
+    return 0;
+}
