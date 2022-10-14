324c918fad33869d4e964fa590db03d9ed8203d1
Aman Bhavsar <96308303+5264A@users.noreply.github.com>
2022-10-14 20:54:22 +0530

    Update solution.cpp

--- print-problem/solution.cpp
+++ print-problem/solution.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout<<"Enter value of n:\t";   
-    int n;  std::cin>>n;                //Taking input from user
+ cout<<"Enter value of n:\t";   
+    int n;  
+    cin>>n;                //Taking input from user
     for(int i=1;i<=n;++i){              //For loop from 1 to n
-        std::cout<<i;                   //priting value of i
+       cout<<i<<" ";                   //priting value of i
     }
     return 0;
 }
